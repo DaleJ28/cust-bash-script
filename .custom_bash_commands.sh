4409ba4f86d77ed096dd547c99da63203c3d5fc2
Dale Jiblal <70293940+DaleJ28@users.noreply.github.com>
2023-09-26 01:39:03 -0400

    fixed control flow,+name,+shift to pass arg&val

--- .custom_bash_commands.sh
+++ .custom_bash_commands.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
-repetitions=1
+greet() {
+    local repetitions=1
+    local name
 
-# greet the user
-function greet() {
+    # Parse arguments
+    while [[ $# -gt 0 ]]; do # '$#' is the argument list to the script, this is while there are more arguments that are greater than 0(-gt 0)
+        key="$1" #imagine that the arguments is an array or list, the $1 refers to the first argument which may be the flag
+        case $key in
+            -n) #here we check if the flag is -n
+                repetitions="$2"
+                #shift is the same as in javascript it removes an item from the start of the list, in this case the processed -n flag with the provided numeric value. (gotcha: ** were not checking if its a number)
+                shift  # past argument
+                shift  # past value
+                ;;
+            *)
+                # Assume the first non-flag argument is the name
+                if [[ -z $name ]]; then
+                    name="$1"
+                    shift  # past argument
+                else
+                    echo "Usage: greet name [-n repetitions]"
+                    return 1
+                fi
+                ;;
+        esac
+    done
 
-# call the function multiple times based on the repetions variable
+    # Ensure a name was provided
+    if [[ -z $name ]]; then  # -z means zero length 
+        echo "Usage: greet name [-n repetitions]"
+        return 1
+    fi
 
-for ((i = 1; i <= repetitions; i++)); do
+    # Print the greeting the specified number of times
+    for ((i = 1; i <= repetitions; i++)); do
+        echo "Hello, $name!"
+    done
+}
 
-echo ‘Hello’ $1‘!’
-	
-
-done
-	 }
-
-
-while getopts "n:" opt; do
-case $opt in
-n) #optarg will take in the number of repetition 
-repetitions="$OPTARG"
-;;
-\?)
-echo "Invalid option: -$OPTARG" >&2 #reject input
-exit 1
-;;
-:)
-echo "Option -$OPTARG requires an argument." >&2
-exit 1
-;;
-esac
-
-done
 
 
 
@@ -42,3 +49,40 @@ function lazygit() {
 		git commit -m "$1"
 		git push origin master
 }
+
+
+#1st attmpt
+
+
+# repetitions=1
+
+# # greet the user
+# function greet() {
+
+# # call the function multiple times based on the repetions variable
+
+# for ((i = 1; i <= repetitions; i++)); do
+
+# echo ‘Hello’ $1‘!’
+	
+
+# done
+# 	 }
+
+
+# while getopts "n:" opt; do
+# case $opt in
+# n) #optarg will take in the number of repetition 
+# repetitions="$OPTARG"
+# ;;
+# \?)
+# echo "Invalid option: -$OPTARG" >&2 #reject input
+# exit 1
+# ;;
+# :)
+# echo "Option -$OPTARG requires an argument." >&2
+# exit 1
+# ;;
+# esac
+
+# done
